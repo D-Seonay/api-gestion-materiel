39e1ed83589404047d35ab70b0e94ca40c77128e
Mathéo DELAUNAY <dseonay@gmail.com>
2025-04-01 10:32:05 +0200

    add modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EquipmentModule } from './equipment/equipment.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, EquipmentModule, ReservationsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
